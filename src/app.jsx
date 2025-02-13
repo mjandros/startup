e819189e0b927781a562eeba0c5a4bf16a656d2e
mjandros <163360746+mjandros@users.noreply.github.com>
2025-02-13 14:46:40 -0700

    Add navigation functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { Rules } from './rules/rules';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
-}
+    return (
+        <BrowserRouter>
+      <div className="body bg-dark text-light">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
+              Simon<sup>&reg;</sup>
+            </div>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+              <NavLink className='nav-link' to=''>Login</NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink className='nav-link' to='play'>Play</NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink className='nav-link' to='leaderboard'>Leaderboard</NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink className='nav-link' to='rules'>Rules</NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/play' element={<Play />} />
+            <Route path='/leaderboard' element={<Leaderboard />} />
+            <Route path='/rules' element={<Rules />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+  
+        <footer className="bg-dark text-white-50">
+          <div className="container-fluid">
+            <span className="text-reset">Author Name(s)</span>
+            <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+      </BrowserRouter>
+    );
+  }
+
+  function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
---
+++ src/rules/rules.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Rules() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>rules displayed here</div>
+    </main>
+  );
+}
