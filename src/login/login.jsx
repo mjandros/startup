c11429186af7f1afb7de5411be98d2fbaacb4584
mjandros <163360746+mjandros@users.noreply.github.com>
2025-02-13 15:03:30 -0700

    Convert login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main>
+            <div>
+                <h1>Blackjack</h1>
+                <form method="get" action="play.html">
+                    <div>
+                    <input type="text" placeholder="Email" />
+                    </div>
+                    <div>
+                    <input type="password" placeholder="Password" />
+                    </div>
+                    <div>
+                        <button type="submit">Login</button>
+                        <button type="submit">Create</button>
+                    </div>
+                </form>
+            </div>
+        </main>
   );
 }
